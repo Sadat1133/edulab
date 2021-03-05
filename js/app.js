aa5483b547ae7ba8a9d2ffdcfa269397def7f0f9
SADAT <you@example.com>
2021-03-05 12:47:03 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    pagination:false,
+    autoplay:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.brand-thumb ').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    pagination:false,
+    autoplay:1000,
+    responsive:{
+        0:{
+            items:4
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+})
